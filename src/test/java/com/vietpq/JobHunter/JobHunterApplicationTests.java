6cd735a63f146c05d6499fb9064638af0ed132de
pqv1991 <pqv1991@gmail.com>
2025-01-18 08:33:03 +0700

    start project

---
+++ src/test/java/com/vietpq/JobHunter/JobHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vietpq.JobHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
